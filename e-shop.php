7f8258abdb04499e72d5d76cb1f279547deff05f
Jun Yang <wpbxwpbx@gmail.com>
2017-05-12 11:49:49 -0400

    PHP team project

--- e-shop.php
+++ e-shop.php
@@ -3,7 +3,7 @@
 session_cache_limiter(false);
 session_start();
 
-require_once '../vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 //DB::$host = '127.0.0.1';
 //OldPassword: ya4Ki9TUSaRATOhq  7qJtprZ2wqz5mqxD
@@ -21,9 +21,9 @@ $app = new \Slim\Slim(array(
 $view = $app->view();
 $view->parserOptions = array(
     'debug' => true,
-    'cache' => dirname(__FILE__) . '/../cache'
+    'cache' => dirname(__FILE__) . '/cache'
 );
-$view->setTemplatesDirectory(dirname(__FILE__) . '/../templates');
+$view->setTemplatesDirectory(dirname(__FILE__) . '/templates');
 
 if (!isset($_SESSION['todouser'])) {
     $_SESSION['todouser'] = array();
