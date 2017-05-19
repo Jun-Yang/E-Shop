c639f28f733b95e0ae66b4acbbd9e4342ad9f66b
DESKTOP-P640TRV\Chen Chen <happygirlcici@hotmail.com>
2017-05-19 02:41:11 -0400

    admin control panel and finish add product

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -1,13 +1,21 @@
 <?php
 
-$app->get('/admin_product', function() use ($app) {
-    $app->render('admin_panel.html.twig',array(
+$app->get('/admin_panel', function() use ($app) {
+    $app->render('admin_panel.html.twig', array(
         "eshopuser" => $_SESSION['eshopuser']
-    ));          
+    ));
+});
+
+$app->get('/admin_product', function() use ($app) {
+    $productList = DB::query("SELECT * FROM products");
+    $app->render('admin_product.html.twig', array(
+        'productList' => $productList,
+        "eshopuser" => $_SESSION['eshopuser']
+    ));
 });
 
 $app->map('/admin_user', function() use ($app) {
-    $userList =  DB::query("SELECT * FROM users");
+    $userList = DB::query("SELECT * FROM users");
     $app->render("admin_user.html.twig", array(
         'userList' => $userList,
         "eshopuser" => $_SESSION['eshopuser']
@@ -22,8 +30,9 @@ $app->map('/admin_user', function() use ($app) {
 //    ));
 //});
 
+
 $app->get('/admin_category', function() use ($app) {
-    $categoryList =  DB::query("SELECT * FROM categories");
+    $categoryList = DB::query("SELECT * FROM categories");
     $app->render("admin_category.html.twig", array(
         'categoryList' => $categoryList,
         "eshopuser" => $_SESSION['eshopuser']
@@ -31,10 +40,77 @@ $app->get('/admin_category', function() use ($app) {
 });
 
 $app->get('/admin_order', function() use ($app) {
-    $orderList =  DB::query("SELECT * FROM orders");
+    $orderList = DB::query("SELECT * FROM orders");
     $app->render("admin_order.html.twig", array(
         'orderList' => $orderList,
         "eshopuser" => $_SESSION['eshopuser']
     ));
 });
 
+$app->get('/admin_product_add', function() use ($app) {
+    $app->render("admin_product_add.html.twig", array(
+        "eshopuser" => $_SESSION['eshopuser']
+    ));
+});
+
+$app->post('/admin_product_add', function() use ($app) {
+    if (!$_SESSION['eshopuser']) {
+        $app->render('forbidden.html.twig');
+        return;
+    }
+//    print_r($_POST);
+    $title = $app->request()->post('title');
+    $catID = $app->request()->post('catID');
+    $modelName = $app->request()->post('modelName');
+    $modelNo = $app->request()->post('modelNo');
+    $desc1 = $app->request()->post('desc1');
+    //$desc2 = $app->request()->post('desc2');
+    //$desc3 = $app->request()->post('desc3');
+    $price = $app->request()->post('price');
+    $stock = $app->request()->post('stock');
+    $discount = $app->request()->post('discount');
+    $today = date("Y-m-d");
+    $postDate = $today;
+
+    $errorList = array();
+    $valueList = array('title' => $title);
+
+    if (strlen($title) < 2 || strlen($title) > 200) {
+        array_push($errorList, "Task name must be 2-100 characters long");
+    } else {
+        $productList = DB::queryFirstRow("SELECT * FROM products WHERE title=%s", $title);
+        if ($productList) {
+            array_push($errorList, "Product title already in use");
+        }
+    }
+
+    $today = date("Y-m-d");
+    if ($postDate < $today) {
+        array_push($errorList, "Post date invalid");
+    }
+//    print_r($_SESSION['todouser']);
+    if ($errorList) {
+        $app->render("admin_product_add.html.twig", ["errorList" => $errorList,
+            'v' => $valueList
+        ]);
+    } else {
+        DB::insert('products', array(
+            "title" => $title,
+            "catID" => $catID,
+            "modelName" => $modelName,
+            "modelNo" => $modelNo,
+            "desc1" => $desc1,
+            "price" => $price,
+            "stock" => $stock,
+            "discount" => $discount,
+            "postDate" => $today
+        ));
+        $app->render("add_success.html.twig", array(
+            "title" => $title,
+            "catID" => $catID,
+            "modelName" => $modelName,
+            "price" => $price,
+            "stock" => $stock
+        ));
+    }
+});
